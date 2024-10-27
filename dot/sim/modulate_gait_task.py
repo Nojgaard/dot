Fix sensor rotation and add observables

dot/sim/modulate_gait_task.py
@@ -12,9 +12,13 @@ from scipy.spatial.transform import Rotation
 import numpy as np
 from numpy.typing import NDArray
 
+from dot.sim.rotation import quat_to_euler
+
 
 class ModulateGaitTask(Task):
-    def __init__(self, model: Quadruped, model_ik: QuadropedIK, model_gait: Gait) -> None:
+    def __init__(
+        self, model: Quadruped, model_ik: QuadropedIK, model_gait: Gait
+    ) -> None:
         self.model = model
         self.model_ik = model_ik
         self.model_gait = model_gait
@@ -24,23 +28,30 @@ class ModulateGaitTask(Task):
         self._arena = floors.Floor(reflectance=0.0)
         self._creature_initial_pose = (0, 0, 0.26)
         self._arena.add_free_entity(self.model)
-        #self._arena.mjcf_model.worldbody.add('light', pos=(0, 0, 4))
+        # self._arena.mjcf_model.worldbody.add('light', pos=(0, 0, 4))
         self.set_timesteps(control_timestep=0.03, physics_timestep=0.005)
 
         # Configure and enable observables
-        #pos_corrptor = noises.Additive(distributions.Normal(scale=0.01))
-        #self._spot.observables.joint_positions.corruptor = pos_corrptor
-        #self._spot.observables.joint_positions.enabled = True
-        #vel_corruptor = noises.Multiplicative(distributions.LogNormal(sigma=0.01))
-        #self._spot.observables.joint_velocities.corruptor = vel_corruptor
-        #self._spot.observables.joint_velocities.enabled = True
+        # pos_corrptor = noises.Additive(distributions.Normal(scale=0.01))
+        # self._spot.observables.joint_positions.corruptor = pos_corrptor
+        # self._spot.observables.joint_positions.enabled = True
+        # vel_corruptor = noises.Multiplicative(distributions.LogNormal(sigma=0.01))
+        # self._spot.observables.joint_velocities.corruptor = vel_corruptor
+        # self._spot.observables.joint_velocities.enabled = True
 
-        self.model.observables.sensors_framequat.enabled = True
-        #self.model.observables.sensors_gyro.enabled = True
-        self.model.observables.sensors_orientation.enabled = True
-        self._task_observables = {}
+        # self.model.observables.sensors_framequat.enabled = True
+        self.model.observables.attitude_velocity.enabled = True
+        self.model.observables.attitude.enabled = True
+        self._task_observables = {
+            "leg_phases": observable.Generic(
+                lambda physics: self.model_gait.leg_phase()
+            )
+        }
+
+        for obs in self._task_observables.values():
+            obs.enabled = True
         self._last_position: NDArray = np.zeros(3)
-    
+
     @property
     def root_entity(self):
         return self._arena
@@ -48,29 +59,31 @@ class ModulateGaitTask(Task):
     @property
     def task_observables(self):
         return self._task_observables
-    
+
     def action_spec(self, physics):
         return super().action_spec(physics)
-    
+
     def before_step(self, physics, action, random_state):
         dt = self.control_timestep
         foot_positions = self.model_gait.compute_foot_positions(dt)
+        print(f"TIME2 {self.model_gait._time}")
         joint_angles = self.model_ik.find_angles(foot_positions)
         super().before_step(physics, joint_angles.flatten(), random_state)
-    
+
     def initialize_episode(self, physics: Physics, random_state):
-        self.model.set_pose(physics, self._creature_initial_pose, np.array([1, 0, 0, 0]))
+        self.model.set_pose(
+            physics, self._creature_initial_pose, np.array([1, 0, 0, 0])
+        )
         joint_names = [j.name for j in self.model.mjcf_model.find_all("joint")]
         for name, angle in zip(joint_names, self._rest_joint_angles):
-           physics.named.data.qpos[f"spot/{name}"] = angle
+            physics.named.data.qpos[f"spot/{name}"] = angle
 
-        self._last_position = self.model.position(physics)
+        self._last_position = np.array(self.model.get_pose(physics)[0])
 
-    
     def get_reward(self, physics: Physics) -> float:
-        position = self.model.position(physics)
-        orientation = self.model.orientation(physics)
-        angular_velocity = self.model.angular_velocity(physics)
+        position, quat = self.model.get_pose(physics)
+        orientation = quat_to_euler(quat)
+        _, angular_velocity = self.model.get_velocity(physics)
         dpos = position - self._last_position
 
         distance_weight = 1.0
@@ -79,10 +92,8 @@ class ModulateGaitTask(Task):
         angular_velocity_weight = 0.05
 
         reward = distance_weight * dpos[0]
-        reward -= drift_weight * position[1]
+        reward -= drift_weight * abs(position[1])
         reward -= orientation_weight * np.sum(np.abs(orientation[:2]))
         reward -= angular_velocity_weight * np.sum(np.abs(angular_velocity[:2]))
-        self._last_position = position
+        self._last_position = np.array(position)
         return reward
-
-        

dot/sim/quadruped.py
@@ -8,6 +8,8 @@ from dm_control import mjcf
 import numpy as np
 from scipy.spatial.transform import Rotation
 
+from dot.sim.rotation import quat_to_euler
+
 
 class Quadruped(Entity):
     def _build(self):
@@ -30,14 +32,6 @@ class Quadruped(Entity):
 
     def _build_observables(self):
         return QuadrupedObservables(self)
-
-    def position(self, physics: Physics):
-        return np.array(physics.bind(self._imu).xpos)
-    
-    def orientation(self, physics: Physics):
-        xmat = physics.bind(self._imu).xmat.reshape((3, 3))
-        rotation = Rotation.from_matrix(xmat)
-        return rotation.as_euler("XYZ")
     
     def angular_velocity(self, physics: Physics):
         return np.array(physics.bind(self.mjcf_model.sensor.gyro).sensordata)
@@ -98,23 +92,22 @@ class QuadrupedObservables(Observables):
         return observable.MJCFFeature("qvel", all_joints)
 
     @composer.observable
-    def sensors_gyro(self):
-        return observable.MJCFFeature("sensordata", self._entity.mjcf_model.sensor.gyro)
+    def attitude_velocity(self):
+        return observable.MJCFFeature("sensordata", self._entity.mjcf_model.sensor.gyro, index=[0, 1])
 
     @composer.observable
-    def sensors_accelerometer(self):
+    def accelerometer(self):
         return observable.MJCFFeature(
             "sensordata", self._entity.mjcf_model.sensor.accelerometer
         )
 
     @composer.observable
-    def sensors_orientation(self):
+    def attitude(self):
         def read_orientation(physics):
             framequat_element = self._entity.mjcf_model.sensor.framequat
-            binding = physics.bind(framequat_element)
-            framequat = getattr(binding, "sensordata")
-            rotation: Rotation = Rotation.from_quat(framequat)
-            return rotation.as_euler("ZYX", degrees=False)
+            quat = physics.bind(framequat_element).sensordata
+            return quat_to_euler(quat)[:2]
+
         return observable.Generic(read_orientation)
 
     @composer.observable